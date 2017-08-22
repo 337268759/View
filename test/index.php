054148e63f16a0a3580a367ad4ea0fc40669084c
Jun-mac <jun.zhou@mobisist.com>
2017-08-22 18:29:37 +0800

    新的版本

--- test/index.php
+++ test/index.php
@@ -2,8 +2,8 @@
 
 include_once "../src/View.php";
 
-$html = "abcd{cms:dj name=\"Jun\" key='index'}{/cms:dj}";
-$html .= "abcd{cms:novel name=\"Jun\" key='index'}{/cms:novel}";
+$html = "abcd{cms:xxx name=\"Jun\" key='index'}{/cms:dj}";
+$html .= "abcd{cms:aaa name=\"Jun\" key='index'}{/cms:novel}";
 
-View::add_tpl_hook('dj', ['field' => 'name,key,value']);
+View::add_tpl_hook('xxx', ['field' => 'name,key,value']);
 View::compile($html);
